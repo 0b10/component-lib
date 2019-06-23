08bb7935399113cb0f35781938005bd1a11d1fec
0b10 <0b10@pm.me>
2019-06-23 04:09:30 +0100

    Make story modules load for storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
+import { configure } from "@storybook/react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+const req = require.context("./", true, /^.+\.stories\.jsx?$/);
+const loadStories = () => req.keys().forEach(filename => req(filename));
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+configure(loadStories, module);
