4148c66eb8a76926364020e3256b5302c567d6d7
0b10 <0b10@pm.me>
2019-07-06 14:47:23 +0100

    Style the fieldeset

--- src/components/form/FieldSet.jsx
+++ src/components/form/FieldSet.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const FieldSet = props => (
-  <fieldset>
-    <legend>{props.legend}</legend>
+  <fieldset style={(props.styles && props.styles.fieldset) || styles.fieldset}>
+    <legend style={(props.style && props.styles.legend) || styles.legend}>
+      {props.legend}
+    </legend>
     {props.children}
   </fieldset>
 );
 
+// FIXME: determine how to use the global theme.
+const styles = {
+  fieldset: {
+    padding: "50px",
+    borderRadius: "4px"
+  },
+  legend: undefined
+};
+
 FieldSet.propTypes = {
-  legend: PropTypes.string
+  //** The legend text */
+  legend: PropTypes.string,
+  styles: PropTypes.shape({
+    fieldset: PropTypes.object,
+    legend: PropTypes.object
+  })
 };
