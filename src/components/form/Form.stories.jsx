6d27ec00be4082e7ac48c5eadf9bdc980aab5670
0b10 <0b10@pm.me>
2019-07-06 12:23:13 +0100

    Move withValidation to it's own module

--- src/components/form/Form.stories.jsx
+++ src/components/form/Form.stories.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { Form, withValidation } from "./Form";
+import { Form } from "./Form";
+import { withValidation } from "./withValidation";
 import * as yup from "yup";
 
 const componentName = "Form";
