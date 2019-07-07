0263a65db8ea549b31e0c1aadf2f8171f4ba150c
0b10 <0b10@pm.me>
2019-07-07 12:59:05 +0100

    Make FieldSet use defaultProps

--- src/components/form/FieldSet.jsx
+++ src/components/form/FieldSet.jsx
@@ -2,22 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const FieldSet = props => (
-  <fieldset style={(props.styles && props.styles.fieldset) || styles.fieldset}>
-    <legend style={(props.style && props.styles.legend) || styles.legend}>
-      {props.legend}
-    </legend>
+  <fieldset style={props.styles.fieldset}>
+    <legend style={props.styles.legend}>{props.legend}</legend>
     {props.children}
   </fieldset>
 );
-
-// FIXME: determine how to use the global theme.
-const styles = {
-  fieldset: {
-    padding: "50px",
-    borderRadius: "4px"
-  },
-  legend: undefined
-};
 
 FieldSet.propTypes = {
   //** The legend text */
@@ -26,4 +15,13 @@ FieldSet.propTypes = {
     fieldset: PropTypes.object,
     legend: PropTypes.object
   })
+};
+
+FieldSet.defaultProps = {
+  styles: {
+    fieldset: {
+      padding: "50px",
+      borderRadius: "4px"
+    }
+  }
 };
