a18e434b4032af51221f2ad677b39f6b2281759c
0b10 <0b10@pm.me>
2019-07-14 13:05:49 +0100

    Make initial package structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+export class Home extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>stub</div>
+      </React.Fragment>
+    );
+  }
+}
+
+Home.propTypes = {};
---
+++ src/pages/Home/__tests__/unit/Home.test.jsx
@@ -0,0 +1,5 @@
+describe("Unit Tests: Home (page)", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
