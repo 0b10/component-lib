595667e114ff3db9ad2fba9b2d81c2bddba5894d
0b10 <0b10@pm.me>
2019-07-15 13:51:51 +0100

    Create unit tests for Post, Posts, SideMenu, SideMenuButton and NavBar in App

--- src/__tests__/unit/App.test.jsx
+++ src/__tests__/unit/App.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import App from "../../App";
 import { mount } from "enzyme";
 import { stubs } from "../../layouts";
+import { stubs as postsStubs } from "../../components";
 
 describe("Unit Tests: App", () => {
   it("should render something, anything", () => {
@@ -9,18 +10,20 @@ describe("Unit Tests: App", () => {
     expect(wrapper.length).toBe(1);
   });
 
-  // describe.each`
-  //   component
-  //   ${"NavBar"}
-  //   ${"SideMenuButton"}
-  //   ${"SideMenu"}
-  // `("$component", ({ component }) => {
-  //   it(`should render`, () => {
-  //     const wrapper = mount(<HomeFactory />).find(component);
-  //     // console.log(wrapper.debug());
-  //     expect(wrapper.length).toBe(1);
-  //   });
-  // });
+  const postsLength = () => postsStubs.posts.posts.getPosts().length;
+
+  describe.each`
+    component           | length
+    ${"NavBar"}         | ${1}
+    ${"SideMenuButton"} | ${1}
+    ${"SideMenu"}       | ${1}
+    ${"Post"}           | ${postsLength()}
+  `("$component", ({ component, length }) => {
+    it(`should be rendered ${length > 1 ? length + " times" : "once"}`, () => {
+      const wrapper = mount(<AppFactory />).find(component);
+      expect(wrapper.length).toBe(length);
+    });
+  });
 });
 
 const AppFactory = () => (
